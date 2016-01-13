668467336366e8fc480f5bf90d67ddb84720966f
Riccardo Zorn <r.zorn@tmg.it>
2016-01-13 13:11:53 +0100

    Version 1.7.1

--- plg_toomanyfiles/README.php
+++ plg_toomanyfiles/README.php
@@ -1,5 +1,4 @@
 <?php
-die();
 /**
  * @package     Too Many Files plugin
  * @copyright   Copyright (C) 2012-2014 Riccardo Zorn. All rights reserved.
@@ -7,13 +6,14 @@ die();
  * @author      Riccardo Zorn <support@tmg.it> - http://www.fasterjoomla.com/too-many-files-optimize-your-site-js-and-css
  */
 defined('_JEXEC') or die;
+die();
 ?>
 
 Too Many Files - Plugin for Joomla Admin
 Homepage: http://www.fasterjoomla.com/
 
 --------------------------------------
-Version 1.1.6 (2013/04/28) alpha 
+Version 1.1.6 (2013/04/28) alpha
 	ATTENTION: This is Alpha software. We're still implementing some basic functionality so it's quite fair to assume it won't work on your site unless it's a fresh Joomla! 2.5 installation with no extra extensions, and even then it may cause trouble.
 	Please contact us if you are interested in the product. Download at your own risk (the worst that can happen though is that you may have to uninstall it).
 
@@ -21,20 +21,20 @@ Version 1.1.6 (2013/04/28) alpha
 	Reduce the number of css and js calls in your page, and load scripts after content.
 	After minification comes compression, .htaccess automatic generation, and is able to serve a pre-compressed copy of your js and css files to all clients who support it.
 	Please visite the extension's page on fasterjoomla.com to learn more and find all reference.
-	
+
 Version 1.2.4 (2013-03-18) alpha
 	First stable version
-	
+
 Version 1.2.5 (2013-05-08) alpha
 	Introduced support for @import styles. It should no longer be necessary to exclude any css files from the join/minify process.
 	Tested on: Joomla 2.5+ - 3.2+, jomsocial, dj-classifieds, rsforms, chronoforms, jomres, sobi, zoo, yootheme/warp,
 	 	rockettheme/gantry, themeforest, foundation, h5bp templates and more, javascript libraries: twitter bootstrap, yui, any jQuery plugin
-	
+
 Version 1.2.6
-	Support several syntaxes: 
+	Support several syntaxes:
 		local files invoked with a param i.e. template/$template/js/somefile.js?ver=11
 		remote js/css files served by php and/or invoked with a param i.e. Google maps api
-	
+
 Version 1.3.0 (2014-01-15) beta
 	Several improvements:
 	- Updated the supported libraries versions
@@ -43,13 +43,13 @@ Version 1.3.0 (2014-01-15) beta
 Version 1.4.0 (2014-02-12) beta
 	Added a configuration option to selectively disable pages based on Menu Itemid.
 	Added a configuration option to select for which user groups the plugin will be enabled
-	
+
 Version 1.4.2 (2014-02-21) beta
-	Now mootools library processing exclusion is enforced also for admin users. 
-	Debug modes enforced to produce more consistent output. 
-	
+	Now mootools library processing exclusion is enforced also for admin users.
+	Debug modes enforced to produce more consistent output.
+
 Version 1.5.0 (2014-02-22) beta
-	New: Removing JComments, JTooltip, keepAlive removes both the libraries and the scripts, so you don't get errors.  
+	New: Removing JComments, JTooltip, keepAlive removes both the libraries and the scripts, so you don't get errors.
 	If mootools Core is removed, these will be all removed since they can't work without it.
 
 Version 1.5.5 (2014-03-05) production
@@ -57,24 +57,24 @@ Version 1.5.5 (2014-03-05) production
 
 Version 1.5.6 (2014-05-31) production
 	Improved support for IE conditional comments.
----------------------------------------	
+---------------------------------------
 
 As of June, 2014 the plugin Too Many Files has been improved to support
 a smarter compression with the new Pro component "Too Many Files Pro".
 This currently does not affect existing features.
 
 In an ongoing effort to keep things as simple as possible for the user,
-the "Too Many Files Pro" scans your site and gathers most of the information 
+the "Too Many Files Pro" scans your site and gathers most of the information
 that is necessary for the configuration, thus making it easier for it to work
 with no manual configuration at all, and to ease testing of custom configurations.
 
 
 Version 1.5.11 (2014-11-11)
 	Improved reliability and compatibility with other extensions
-	most notably excluded the creation of the custom 
-	.htaccess in the css4min cache folder to prevent 
+	most notably excluded the creation of the custom
+	.htaccess in the css4min cache folder to prevent
 	incompatibilities with Admin Tools by Akeeba.
-	
+
 Version 1.6.1 (2014-12-02)
 *    Added extra configuration params to specify the exact version and source of libraries
     The configuration is backwards compatible: i.e. your previous settings will continue
@@ -95,17 +95,17 @@ Version 1.6.2 (2015-03-16)
             modules/assets/something.js
         instead of
             /modules/assets/something.js
-            
+
 Version 1.6.3 (2015-03-18)
 *   fix jQuery compatibility issues.
         Just enable the flag "Fix $-jQuery Error" in the plugin Header Management options.
-        
+
         Some scripts rely on the prefix $ being available;
-        however, it is customary to load jQuery invoking noConflict() in Joomla!  
+        however, it is customary to load jQuery invoking noConflict() in Joomla!
         This option re-assigns $ to jQuery, so further scripts can rely on $ being jQuery.
 Version 1.6.4 (2015-03-20, eclipse day)
     Fixed a nasty typo
-       
+
 Version 1.6.5 (2015-11-11)
 *	Support for an "apparently" local file which doesn't exist i.e.
       /default/css.html, with css content, being generated by a plugin.
@@ -113,12 +113,11 @@ Version 1.6.5 (2015-11-11)
 NOTE : Joomla 3 compatibility only!!! A future version will restore 2.5 compatibility,
 	but in order to speed up the integration with the TooManyFilesPro component faster,
 	the next few versions will be 3.x only.
-	
+
 Version 1.7.0 (2015-12-12)
 *	<b>Fix even MORE jQuery compatibility issues!</b> with support for jQuery.noConflict()
 	which can be disabled, added, or ignored in the plugin's options.
 	In case it is removed or added, all internal and external calls are removed
 	and the single noConflict() call is added right after jQuery if added.
 	The new setting's default value is 'Leave as Is' for legacy compatibility.
-	
-	
+
--- plg_toomanyfiles/elements/zzlibrary.php
+++ plg_toomanyfiles/elements/zzlibrary.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     FasterJoomla Admin components - Toomanyfiles
- * 
+ *
  * This package shows an editable drop-down to the user with content like:
  *  - No
  *  - CDN
@@ -11,7 +11,7 @@
  *  	- js/jquery.1.10.3.min.js
  *  	- js/jquery.2.1.0.min.js
  *  - a custom element if appropriate;
- *  
+ *
  *  The user may specify an alternate local or remote version for each library.
  *
  * @author Riccardo Zorn support@fasterjoomla.com
@@ -19,34 +19,32 @@
  * @license GNU/GPL v2 or greater http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-defined('_JEXEC') or die;
-
-defined('_JEXEC') or die('Error');
+defined('_JEXEC') or die('Error Joomla required');
 
 //2.5? jimport('joomla.form.formfield');
 JFormHelper::loadFieldClass('groupedlist');
 
 class JFormFieldZzlibrary extends JFormFieldGroupedList
 {
-	
+
     protected $type = 'zzlibrary';
-    
+
     protected $directory;
     protected $cssClass;
-    
+
     protected function getInput() {
     	// Including fallback code for HTML5 non supported browsers.
     	JHtml::_('jquery.framework');
     	JHtml::_('script', 'system/html5fallback.js', false, true);
     	JHtml::_('behavior.modal', 'a.modal');
-    	
+
     	$class="libclass";
     	$title = "title";
     	$body = parent::getInput();
     	$document = JFactory::getDocument();
     	$inputid = $this->type . '_field_'.$this->element["name"];
     	$selectid = 'jform_params_'.$this->element["name"];
-    	
+
     	$document->addScriptDeclaration('
     			/**
     			 * This will update the href of the test button:
@@ -58,17 +56,17 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
     			jQuery(function($) {
     				// initialize:
     				'.$inputid.'_changed($("#'.$inputid.'").val());
-    			
+
     				// when typing in a new uri
     				$("#'.$inputid.'").change(function() {
     					'.$inputid.'_changed(this.value);
-    			
+
     					// now let\'s update the select\'s custom option or create a new custom option;
     					customSelector = "#'.$selectid.' .' .$this->cssClass . '";
     					$customOption = jQuery(customSelector);
-    					if ($customOption.length==0) { 
+    					if ($customOption.length==0) {
     						// create a custom option to hold the edited uri
-    						jQuery("#'.$selectid.'").append("<option value=\"1\" class=\"' . 
+    						jQuery("#'.$selectid.'").append("<option value=\"1\" class=\"' .
     							$this->cssClass . '\">loading...</option>");
     						$customOption = jQuery(customSelector);
     					}
@@ -77,9 +75,9 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
     					$customOption.text(this.value);
     					$customOption.attr("selected","selected");
     					// now update the chosen markup for the nice dropdown
-    					jQuery("#'.$selectid.'").trigger("liszt:updated");			
+    					jQuery("#'.$selectid.'").trigger("liszt:updated");
     				});
-    			
+
     				// when selecting a different option from the dropdown
     			    $("#'.$selectid.'").change(function() {
     					if (this.value=="0" || this.value=="-1" || this.value=="1") {
@@ -89,22 +87,22 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
     						jQuery("#'.$inputid.'").val(this.value);
     						'.$inputid.'_changed(this.value);
     					}
-    				}); 			
-    			});    			
+    				});
+    			});
     			');
     	$href=$this->value;
     	$hidden = in_array($href, array("-1","0","1"))?'hidden':'';
 		return "<div class='zzlibrary $class'>
-				<div class='body'>$body</div> 
+				<div class='body'>$body</div>
 				<div class='test $hidden' id='container_$inputid'>
 					<input type='text' id='$inputid' value='$href' />
 					<a class='btn btn-small btn-success modal' rel='{handler: \"iframe\"}' id='test_$inputid' >Test</a>
-				</div> 
+				</div>
 			</div>";
 	}
-	
+
 	/**
-	 * Method to edit the SimpleXMLElement from the configuration xml 
+	 * Method to edit the SimpleXMLElement from the configuration xml
 	 * adding the extra values for the local files and - if necessary - the selected item.
 	 *
 	 * @return  array  The field option objects as a nested array in groups.
@@ -135,26 +133,26 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 						}
 						break;
 					case 'local':
-						if (!$foundCurrentValue && $value){ 
+						if (!$foundCurrentValue && $value){
 							$foundCurrentValue = $this->addLocalFiles($element);
 						} else {
 							$this->addLocalFiles($element);
 						}
 						break;
 				}
-			}			
+			}
 		}
 		if (!$foundCurrentValue && $value) {
-			
+
 			$el = $this->element->addChild('option',$value);
 			$el->addAttribute('value',$value);
-			
+
 			$el->addAttribute('class',$this->cssClass);
 			$el->addAttribute('label','Custom source: '.$value);
 		}
 		return parent::getGroups();
 	}
-	
+
 	/**
 	 * Browse folder for libraries.
 	 * @param SimpleXMLElement $element
@@ -164,7 +162,7 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 		$filefilter = $this->element['filefilter'];
 		$value = $this->value;
 		$foundCurrentValue = false;
-		
+
 		if (empty($directory))
 			return $foundCurrentValue;
 		if (strpos($directory,'/')===0) {
@@ -173,7 +171,7 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 			$path = JPATH_SITE.'/plugins/system/toomanyfiles/'.$directory . '/';
 		}
 		jimport('joomla.filesystem.folder');
-		
+
 		$files = JFolder::files($path,$filefilter,false,false,	array(),	array(),true);
 		if (is_array($files)) {
 			$rel_path = str_replace(JPATH_SITE,'',$path);
@@ -185,10 +183,10 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 				}
 			}
 		}
-		
+
 		return $foundCurrentValue;
 	}
-	
+
 	/**
 	 * Method to attach a JForm object to the field.
 	 *
@@ -206,19 +204,19 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 	public function setup(SimpleXMLElement $element, $value, $group = null)
 	{
 		$this->addCommonStylesAndScripts();
-		
+
 		$return = parent::setup($element, $value, $group);
-	
+
 		if ($return)
 		{
 			$this->directory = rtrim( (string) $this->element['directory'],'/');
 		}
-		
+
 		$this->cssClass = $this->type . '_class_'.$this->element["name"];
-		
+
 		return $return;
 	}
-	
+
 	protected function addCommonStylesAndScripts() {
 		if (!defined('_toomanyfiles_zzlibrary')) {
 			define('_toomanyfiles_zzlibrary','1');
@@ -250,5 +248,5 @@ class JFormFieldZzlibrary extends JFormFieldGroupedList
 				}
 			');
 		}
-	} 
+	}
 }
